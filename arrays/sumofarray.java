4d1d9a14fd03f3edc9f9f1a3afb3a0944e9c2608
Navya Sri Maroju <marojunavyasrii@gmail.com>
2026-09-08 19:16:30 +0530

    Update sumofarray.java

--- arrays/sumofarray.java
+++ arrays/sumofarray.java
@@ -1,1 +1,12 @@
+class sumofarray {
+  public static void main(String[] args) {
+    int[] arr = {10,20,40,68,21};
+    int sum = 0;
+    for(int i=0;i<arr.length; i++) {
+      sum = sum + arr[i];
+    }
+    System.out.println("sum of elements:" +sum);  
+  }
+}
 
+//output: sum of elements:159
