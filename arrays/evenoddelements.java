4a89a07b3220c4344f9f0bb73be7f2ee010c8c90
Navya Sri Maroju <marojunavyasrii@gmail.com>
2026-09-08 19:50:40 +0530

    Update and rename evenoddelements to evenoddelements.java

---
+++ arrays/evenoddelements.java
@@ -0,0 +1,21 @@
+class evenoddelements {
+    public static void main(String[] args) {
+
+        int num[] = {2, 28, 20, 63, 30,42};
+
+        String even = "";
+        String odd = "";
+
+        for (int i = 0; i < num.length; i++) {
+
+            if (num[i] % 2 == 0) {
+                even = even + num[i] + " "; //" is used to concatenate the even numbers in the string"
+            } else {
+                odd = odd + num[i] + " ";
+            }
+        }
+
+        System.out.println("Even numbers are:"+even);
+        System.out.println("Odd numbers are:"+odd);
+    }
+}
