c4e4fb6ac57080ee1a5734fb95470cc6a43c3401
Navya Sri Maroju <marojunavyasrii@gmail.com>
2026-09-08 19:28:16 +0530

    Implement logic to find largest element in array
    Fix the logic to find the largest element in the array.

--- arrays/largestelement.java
+++ arrays/largestelement.java
@@ -1,1 +1,14 @@
+class largestelement {
+  public static void main(String[] args) {
+    int a[] = {100,50,200,40,90};
+    int largest = a[0];
+    for(int i=0;i<a.length;i++){
+      if(a[i]>largest){
+        largest = a[i];
+      }
+    }
+    System.out.println("The largest element in array is:"+largest);
+  }
+}
 
+//output:The largest element in array is:200
