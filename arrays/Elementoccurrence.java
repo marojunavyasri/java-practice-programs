dd239b002ae423a4a732e1b065e33996e5b1a86c
Navya Sri Maroju <marojunavyasrii@gmail.com>
2026-09-08 21:32:36 +0530

    Create  Elementoccurrence.java

---
+++ arrays/Elementoccurrence.java
@@ -0,0 +1,23 @@
+class Elementoccurrence {
+
+    public static void main(String[] args) {
+
+        int num[] = {1, 2, 2, 2, 3, 4, 4};
+
+        for (int i = 0; i < num.length; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < num.length; j++) {
+
+                if (num[i] == num[j]) {
+                    count++;
+                }
+            }
+
+            if (count > 1) {
+                System.out.println(num[i] + " occurs " + count + " times");
+            }
+        }
+    }
+}
