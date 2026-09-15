4584903c077a85ce2198f1981b5569bb5c4b347e
Navya Sri Maroju <marojunavyasrii@gmail.com>
2026-09-16 01:58:28 +0530

    Implement Vehicle and Car classes with polymorphism

---
+++ polymorphism/Vehicle.java
@@ -0,0 +1,19 @@
+class Vehicle {
+
+    void start() {
+        System.out.println("Vehicle starts");
+    }
+}
+
+class Car extends Vehicle {
+
+    void start() {
+        System.out.println("Car starts with a key");
+    }
+
+    public static void main(String[] args) {
+
+        Vehicle v = new Car();
+        v.start();
+    }
+}
